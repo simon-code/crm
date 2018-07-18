2c5e5e54123dee7889e74308d51cb59c62960677
simon-code <simon.tang.arias@gmail.com>
2018-07-18 17:10:13 -0400

    crm contact class

---
+++ contact.rb
@@ -0,0 +1,16 @@
+class Contact
+
+attr_accessor :first_name, :last_name, :email, :note
+
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
+end
